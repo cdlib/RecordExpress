f5cf53523d545110fcc1e6d688c70f72f5f16ad2
Mark Redar <mredar@gmail.com>
2013-03-06 10:54:21 -0800

    Make helper scripts better

--- test_project/test-my-apps.sh
+++ test_project/test-my-apps.sh
@@ -1,1 +1,3 @@
-python manage.py test -v2 dublincore collection_record
+#! /usr/bin/env bash
+scriptdir=`dirname "${BASH_SOURCE}"`
+python ${scriptdir}/manage.py test -v2 dublincore collection_record
